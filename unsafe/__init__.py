46c9a2049aab3d4127cb9d950099221a981608ed
ahur4 <95452522+ahur4@users.noreply.github.com>
2024-10-08 11:40:06 +0330

    addeed __all__ and __author__ vars

--- unsafe/__init__.py
+++ unsafe/__init__.py
@@ -1,6 +1,21 @@
-from .ext import forensic
-from .ext import web
-from .ext import exploits
-from .ext import cryptography
-from .ext import network
+import warnings
+
 from .ext import bruteforce
+from .ext import cryptography
+from .ext import exploits
+from .ext import forensic
+from .ext import network
+from .ext import web
+
+warnings.filterwarnings('ignore')
+
+__all__ = [
+    'bruteforce',
+    'cryptography',
+    'exploits',
+    'forensic',
+    'network',
+    'web'
+]
+
+__author__ = "Ahur4"
